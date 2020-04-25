9c55fee7ba9dc04a13c23d26346e47040453b366
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-04-25 10:43:46 +0600

    Adding Security Utility

---
+++ src/main/java/com/eCommerce/utility/SecurityUtility.java
@@ -0,0 +1,32 @@
+package com.eCommerce.utility;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityUtility {
+	private static final String SALT = "salt"; // Salt should be protected carefully
+	
+	@Bean
+	public static BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+	}
+	
+	@Bean
+	public static String randomPassword() {
+		String SALTCHARS = "ABCEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+		StringBuilder salt = new StringBuilder();
+		Random rnd = new Random();
+		
+		while (salt.length()<18) {
+			int index= (int) (rnd.nextFloat()*SALTCHARS.length());
+			salt.append(SALTCHARS.charAt(index));
+		}
+		String saltStr = salt.toString();
+		return saltStr;
+	}
+}
