3771160aeba47abbe7e2f56c0a61549faf632b93
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-04-25 10:42:55 +0600

    Adding UserRepository files

---
+++ src/main/java/com/eCommerce/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.eCommerce.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.eCommerce.domain.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+	User findByUsername(String username);
+}
