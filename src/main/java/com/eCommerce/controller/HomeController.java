2aa11f91d326b3ea0566cb8a50108e803d3c6e2e
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-04-25 10:45:09 +0600

    Updating HomeController

--- src/main/java/com/eCommerce/controller/HomeController.java
+++ src/main/java/com/eCommerce/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.eCommerce.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +14,24 @@ public class HomeController {
 	
 	@RequestMapping("/myAccount")
 	public String myAccount() {
+		return "myAccount";
+	}
+	
+	@RequestMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("classActiveLogin", true);
+		return "myAccount";
+	}
+	
+	@RequestMapping("/forgetPassword")
+	public String forgetPassword(Model model) {
+		model.addAttribute("classActiveForgetPassword", true);
+		return "myAccount";
+	}
+	
+	@RequestMapping("/newUser")
+	public String newUser(Model model) {
+		model.addAttribute("classActiveNewUser", true);
 		return "myAccount";
 	}
 }
