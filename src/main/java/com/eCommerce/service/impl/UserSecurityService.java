cfd0f15df0eb02e48937174602d0bd1832019c9e
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-04-25 10:40:46 +0600

    Adding Service implementation

---
+++ src/main/java/com/eCommerce/service/impl/UserSecurityService.java
@@ -0,0 +1,29 @@
+package com.eCommerce.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.eCommerce.domain.User;
+import com.eCommerce.repository.UserRepository;
+
+@Service
+public class UserSecurityService implements UserDetailsService{
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		
+		if(null == user) {
+			throw new UsernameNotFoundException("Username not found");
+		}
+		
+		return user;
+	}
+
+}
