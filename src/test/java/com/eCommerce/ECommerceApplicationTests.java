12749ffdb8d1dee68237a086ada7dca7fad78f83
Foysal Mahmud <foysal.bu.cse28@gmail.com>
2020-04-25 10:41:30 +0600

    n/a

--- src/test/java/com/eCommerce/ECommerceApplicationTests.java
+++ src/test/java/com/eCommerce/ECommerceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bookstore;
+package com.eCommerce;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BookstoreApplicationTests {
+public class ECommerceApplicationTests {
 
 	@Test
 	public void contextLoads() {
