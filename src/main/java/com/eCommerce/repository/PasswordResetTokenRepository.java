7b458de8ddd0d0b694acb554d250c955fd970366
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-04-27 20:56:43 +0600

    Adding PasswordResetTokenRepository files

---
+++ src/main/java/com/eCommerce/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,25 @@
+package com.eCommerce.repository;
+
+import java.util.Date;
+import java.util.stream.Stream;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import com.eCommerce.domain.User;
+import com.eCommerce.domain.security.PasswordResetToken;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+	
+	PasswordResetToken findByToken(String token);
+	
+	PasswordResetToken findByUser(User user);
+	
+	Stream<PasswordResetToken> findAllByExpiryDateLessThan(Date now);
+	
+	@Modifying
+	@Query("delete from PasswordResetTOken t where t.expirydate <= ?1")
+	void deleteAllExpiredSince(Date now);
+
+}
