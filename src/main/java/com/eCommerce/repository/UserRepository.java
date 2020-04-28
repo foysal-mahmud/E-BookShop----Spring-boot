df308dac029bde74f8172450c1c660d63d277d6d
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-04-28 19:23:05 +0600

    Update UserRepository.java

--- src/main/java/com/eCommerce/repository/UserRepository.java
+++ src/main/java/com/eCommerce/repository/UserRepository.java
@@ -6,4 +6,6 @@ import com.eCommerce.domain.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 	User findByUsername(String username);
+	
+	User findByEmail(String email);
 }
