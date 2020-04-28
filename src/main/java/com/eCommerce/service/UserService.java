10c9aae62fc2b6f57b9bc3e5dd908e6e65a7a9c8
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-04-28 19:21:32 +0600

    Update UserService.java

--- src/main/java/com/eCommerce/service/UserService.java
+++ src/main/java/com/eCommerce/service/UserService.java
@@ -7,4 +7,8 @@ public interface UserService {
 	PasswordResetToken getPasswordResetToken(final String token);
 	
 	void createPasswordResetTokenForUser(final User user, final String token);
+	
+	User findByUsername(String username);
+	
+	User findByEmail (String email);
 }
