f38cd894cf7a9187f88cabb9764b8a08986e5150
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-02 19:40:14 +0600

    Update UserServiceImpl.java

--- src/main/java/com/eCommerce/service/impl/UserServiceImpl.java
+++ src/main/java/com/eCommerce/service/impl/UserServiceImpl.java
@@ -1,19 +1,31 @@
 package com.eCommerce.service.impl;
 
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.eCommerce.repository.PasswordResetTokenRepository;
-import com.eCommerce.repository.UserRepository;
 import com.eCommerce.domain.User;
 import com.eCommerce.domain.security.PasswordResetToken;
+import com.eCommerce.domain.security.UserRole;
+import com.eCommerce.repository.PasswordResetTokenRepository;
+import com.eCommerce.repository.RoleRepository;
+import com.eCommerce.repository.UserRepository;
 import com.eCommerce.service.UserService;
+
 
 @Service
 public class UserServiceImpl implements UserService{
 	
+	private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
 	
 	@Autowired
 	private PasswordResetTokenRepository passwordResetTokenRepository;
@@ -34,9 +46,33 @@ public class UserServiceImpl implements UserService{
 		return userRepository.findByUsername(username);
 	}
 	
+	@Override
 	public User findByEmail (String email) {
 		return userRepository.findByEmail(email);
 	}
-
+	
+	@Override
+	public User createUser(User user, Set<UserRole> userRoles){
+		User localUser = userRepository.findByUsername(user.getUsername());
+		
+		if(localUser != null) {
+			LOG.info("user {} already exists. Nothing will be done.", user.getUsername());
+		} else {
+			for (UserRole ur : userRoles) {
+				roleRepository.save(ur.getRole());
+			}
+			
+			user.getUserRoles().addAll(userRoles);
+			
+			localUser = userRepository.save(user);
+		}
+		
+		return localUser;
+	}
+	
+	@Override
+	public User save(User user) {
+		return userRepository.save(user);
+	}
 
 }
