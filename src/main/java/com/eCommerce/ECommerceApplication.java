62f518170dc558661009b948445f6bf6d1dbc07c
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-02 19:46:29 +0600

    Adding 1 local user for testing

--- src/main/java/com/eCommerce/ECommerceApplication.java
+++ src/main/java/com/eCommerce/ECommerceApplication.java
@@ -1,12 +1,44 @@
 package com.eCommerce;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.eCommerce.domain.User;
+import com.eCommerce.domain.security.Role;
+import com.eCommerce.domain.security.UserRole;
+import com.eCommerce.service.UserService;
+import com.eCommerce.utility.SecurityUtility;
+
+
 @SpringBootApplication
-public class ECommerceApplication {
+public class ECommerceApplication implements CommandLineRunner {
+	
+	@Autowired
+	private UserService userService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		User user1 = new User();
+		user1.setFirstName("John");
+		user1.setLastName("Adams");
+		user1.setUsername("j");
+		user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
+		user1.setEmail("JAdams@gmail.com");
+		Set<UserRole> userRoles = new HashSet<>();
+		Role role1= new Role();
+		role1.setRoleId(1);
+		role1.setName("ROLE_USER");
+		userRoles.add(new UserRole(user1, role1));
+		
+		userService.createUser(user1, userRoles);
 	}
 }
