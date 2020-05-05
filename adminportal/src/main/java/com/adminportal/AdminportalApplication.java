0fd65e1a55a056d2ed543f4b1ad334e264d58cd2
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-05 21:01:00 +0600

    Update AdminportalApplication.java

--- adminportal/src/main/java/com/adminportal/AdminportalApplication.java
+++ adminportal/src/main/java/com/adminportal/AdminportalApplication.java
@@ -1,12 +1,41 @@
 package com.adminportal;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.adminportal.domain.User;
+import com.adminportal.domain.security.Role;
+import com.adminportal.domain.security.UserRole;
+import com.adminportal.service.UserService;
+import com.adminportal.utility.SecurityUtility;
+
 @SpringBootApplication
-public class AdminportalApplication {
+public class AdminportalApplication implements CommandLineRunner{
+	
+	@Autowired
+	private UserService userService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminportalApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		User user1 = new User();
+		user1.setUsername("admin");
+		user1.setPassword(SecurityUtility.passwordEncoder().encode("admin"));
+		user1.setEmail("admin@gmail.com");
+		Set<UserRole> userRoles = new HashSet<>();
+		Role role1= new Role();
+		role1.setRoleId(0);
+		role1.setName("ROLE_ADMIN");
+		userRoles.add(new UserRole(user1, role1));
+		
+		userService.createUser(user1, userRoles);
 	}
 }
