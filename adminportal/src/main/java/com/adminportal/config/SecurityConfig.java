ef54a109aab9efd1532c9d3b9da232c632d86be8
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-05 21:04:44 +0600

    Add files via upload

---
+++ adminportal/src/main/java/com/adminportal/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.adminportal.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.adminportal.service.impl.UserSecurityService;
+import com.adminportal.utility.SecurityUtility;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private Environment env;
+
+	@Autowired
+	private UserSecurityService userSecurityService;
+
+	private BCryptPasswordEncoder passwordEncoder() {
+		return SecurityUtility.passwordEncoder();
+	}
+
+	private static final String[] PUBLIC_MATCHERS = {
+			"/css/**",
+			"/js/**",
+			"/image/**",
+			"/newUser",
+			"/forgetPassword",
+			"/login",
+			"/fonts/**"
+	};
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests().
+		/*	antMatchers("/**").*/
+			antMatchers(PUBLIC_MATCHERS).
+			permitAll().anyRequest().authenticated();
+
+		http
+			.csrf().disable().cors().disable()
+			.formLogin().failureUrl("/login?error")
+			.defaultSuccessUrl("/")
+			.loginPage("/login").permitAll()
+			.and()
+			.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutSuccessUrl("/?logout").deleteCookies("remember-me").permitAll()
+			.and()
+			.rememberMe();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+	}
+
+}
