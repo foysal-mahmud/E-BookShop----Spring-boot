182db89130b21bfcb46ebd818224efa1d6ce9223
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-06 20:10:21 +0600

    Update BookService.java

--- adminportal/src/main/java/com/adminportal/service/BookService.java
+++ adminportal/src/main/java/com/adminportal/service/BookService.java
@@ -1,9 +1,12 @@
 package com.adminportal.service;
 
+import java.util.List;
+
 import com.adminportal.domain.Book;
 
 public interface BookService {
 	
 	Book save(Book book);
 
+	List<Book> findAll();
 }
