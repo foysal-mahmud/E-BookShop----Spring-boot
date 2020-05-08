1d6f3d25ad294b4f08d39eb31f040cb3afe5493f
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-08 21:00:00 +0600

    Update HomeController.java

--- src/main/java/com/eCommerce/controller/HomeController.java
+++ src/main/java/com/eCommerce/controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.eCommerce.controller;
 
+import java.security.Principal;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
@@ -32,7 +36,6 @@ import com.eCommerce.service.UserService;
 import com.eCommerce.service.impl.UserSecurityService;
 import com.eCommerce.utility.MailConstructor;
 import com.eCommerce.utility.SecurityUtility;
-
 
 
 @Controller
@@ -70,6 +73,28 @@ public class HomeController {
 		model.addAttribute("bookList", bookList);
 		
 		return "bookshelf";
+	}
+	
+	@RequestMapping("/bookDetail")
+	public String bookDetail(
+			@PathParam("id") Long id, Model model, Principal principal
+			) {
+		if(principal != null) {
+			String username = principal.getName();
+			User user = userService.findByUsername(username);
+			model.addAttribute("user", user);
+		}
+		
+		Book book = bookService.findById(id);
+		
+		model.addAttribute("book", book);
+		
+		List<Integer> qtyList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		
+		model.addAttribute("qtyList", qtyList);
+		model.addAttribute("qty", 1);
+		
+		return "bookDetail";
 	}
 
 	@RequestMapping("/forgetPassword")
