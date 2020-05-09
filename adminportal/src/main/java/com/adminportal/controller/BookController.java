2b9ad7188d59fd5dbbb060503148a03541594fe3
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-09 19:27:49 +0600

    Update BookController.java

--- adminportal/src/main/java/com/adminportal/controller/BookController.java
+++ adminportal/src/main/java/com/adminportal/controller/BookController.java
@@ -3,6 +3,8 @@ package com.adminportal.controller;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.adminportal.domain.Book;
@@ -50,6 +53,49 @@ public class BookController {
 		}
 
 		return "redirect:bookList";
+	}
+	
+	@RequestMapping("/bookInfo")
+	public String bookInfo(@RequestParam("id") Long id, Model model) {
+		Book book = bookService.findById(id);
+		model.addAttribute("book", book);
+		
+		return "bookInfo";
+	}
+	
+	
+	@RequestMapping("/updateBook")
+	public String updateBook(@RequestParam("id") Long id, Model model) {
+		Book book = bookService.findById(id);
+		model.addAttribute("book", book);
+		
+		return "updateBook";
+	}
+	
+	
+	@RequestMapping(value="/updateBook", method=RequestMethod.POST)
+	public String updateBookPost(@ModelAttribute("book") Book book, HttpServletRequest request) {
+		bookService.save(book);
+		
+		MultipartFile bookImage = book.getBookImage();
+		
+		if(!bookImage.isEmpty()) {
+			try {
+				byte[] bytes = bookImage.getBytes();
+				String name = book.getId() + ".png";
+				
+				Files.delete(Paths.get("src/main/resources/static/image/book/"+name));
+				
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(new File("src/main/resources/static/image/book/" + name)));
+				stream.write(bytes);
+				stream.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return "redirect:/book/bookInfo?id="+book.getId();
 	}
 	
 	@RequestMapping("/bookList")
