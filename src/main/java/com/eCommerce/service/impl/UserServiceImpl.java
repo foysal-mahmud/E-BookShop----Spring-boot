6bb9a0e9991f658b519b2cd5cc566c7314c18e79
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-14 20:27:08 +0600

    Update UserServiceImpl.java

--- src/main/java/com/eCommerce/service/impl/UserServiceImpl.java
+++ src/main/java/com/eCommerce/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.eCommerce.service.impl;
 
+import java.util.List;
 import java.util.Set;
 
 import org.slf4j.Logger;
@@ -8,11 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eCommerce.domain.User;
+import com.eCommerce.domain.UserBilling;
+import com.eCommerce.domain.UserPayment;
+import com.eCommerce.domain.UserShipping;
 import com.eCommerce.domain.security.PasswordResetToken;
 import com.eCommerce.domain.security.UserRole;
 import com.eCommerce.repository.PasswordResetTokenRepository;
 import com.eCommerce.repository.RoleRepository;
+import com.eCommerce.repository.UserPaymentRepository;
 import com.eCommerce.repository.UserRepository;
+import com.eCommerce.repository.UserShippingRepository;
 import com.eCommerce.service.UserService;
 
 
@@ -26,6 +32,12 @@ public class UserServiceImpl implements UserService{
 	
 	@Autowired
 	private RoleRepository roleRepository;
+	
+	@Autowired
+	private UserPaymentRepository userPaymentRepository;
+	
+	@Autowired
+	private UserShippingRepository userShippingRepository;
 	
 	@Autowired
 	private PasswordResetTokenRepository passwordResetTokenRepository;
@@ -74,5 +86,55 @@ public class UserServiceImpl implements UserService{
 	public User save(User user) {
 		return userRepository.save(user);
 	}
+	
+	@Override
+	public void updateUserBilling(UserBilling userBilling, UserPayment userPayment, User user) {
+		userPayment.setUser(user);
+		userPayment.setUserBilling(userBilling);
+		userPayment.setDefaultPayment(true);
+		userBilling.setUserPayment(userPayment);
+		user.getUserPaymentList().add(userPayment);
+		save(user);
+	}
+	
+	@Override
+	public void updateUserShipping(UserShipping userShipping, User user){
+		userShipping.setUser(user);
+		userShipping.setUserShippingDefault(true);
+		user.getUserShippingList().add(userShipping);
+		save(user);
+	}
+	
+	@Override
+	public void setUserDefaultPayment(Long userPaymentId, User user) {
+		List<UserPayment> userPaymentList = (List<UserPayment>) userPaymentRepository.findAll();
+		
+		for (UserPayment userPayment : userPaymentList) {
+			
+			if(userPayment.getId() == userPaymentId) {
+				userPayment.setDefaultPayment(true);
+				userPaymentRepository.save(userPayment);
+			} else {
+				userPayment.setDefaultPayment(false);
+				userPaymentRepository.save(userPayment);
+			}
+		}
+	}
+	
+	@Override
+	public void setUserDefaultShipping(Long userShippingId, User user) {
+		List<UserShipping> userShippingList = (List<UserShipping>) userShippingRepository.findAll();
+		
+		for (UserShipping userShipping : userShippingList) {
+			if(userShipping.getId() == userShippingId) {
+				userShipping.setUserShippingDefault(true);
+				userShippingRepository.save(userShipping);
+			} else {
+				userShipping.setUserShippingDefault(false);
+				userShippingRepository.save(userShipping);
+			}
+		}
+	}
+
 
 }
