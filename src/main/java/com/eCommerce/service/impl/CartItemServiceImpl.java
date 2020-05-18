9b67c2364df6a2d879c1930aacb32cbcc0b971d6
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-18 20:52:20 +0600

    Add files via upload

---
+++ src/main/java/com/eCommerce/service/impl/CartItemServiceImpl.java
@@ -0,0 +1,79 @@
+package com.eCommerce.service.impl;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eCommerce.domain.Book;
+import com.eCommerce.domain.BookToCartItem;
+import com.eCommerce.domain.CartItem;
+import com.eCommerce.domain.ShoppingCart;
+import com.eCommerce.domain.User;
+import com.eCommerce.repository.BookToCartItemRepository;
+import com.eCommerce.repository.CartItemRepository;
+import com.eCommerce.service.CartItemService;
+
+@Service
+public class CartItemServiceImpl implements CartItemService{
+	
+	@Autowired
+	private CartItemRepository cartItemRepository;
+	
+	@Autowired
+	private BookToCartItemRepository bookToCartItemRepository;
+	
+	public List<CartItem> findByShoppingCart(ShoppingCart shoppingCart) {
+		return cartItemRepository.findByShoppingCart(shoppingCart);
+	}
+	
+	public CartItem updateCartItem(CartItem cartItem) {
+		BigDecimal bigDecimal = new BigDecimal(cartItem.getBook().getOurPrice()).multiply(new BigDecimal(cartItem.getQty()));
+		
+		bigDecimal = bigDecimal.setScale(2, BigDecimal.ROUND_HALF_UP);
+		cartItem.setSubtotal(bigDecimal);
+		
+		cartItemRepository.save(cartItem);
+		
+		return cartItem;
+	}
+	
+	public CartItem addBookToCartItem(Book book, User user, int qty) {
+		List<CartItem> cartItemList = findByShoppingCart(user.getShoppingCart());
+		
+		for (CartItem cartItem : cartItemList) {
+			if(book.getId() == cartItem.getBook().getId()) {
+				cartItem.setQty(cartItem.getQty()+qty);
+				cartItem.setSubtotal(new BigDecimal(book.getOurPrice()).multiply(new BigDecimal(qty)));
+				cartItemRepository.save(cartItem);
+				return cartItem;
+			}
+		}
+		
+		CartItem cartItem = new CartItem();
+		cartItem.setShoppingCart(user.getShoppingCart());
+		cartItem.setBook(book);
+		
+		cartItem.setQty(qty);
+		cartItem.setSubtotal(new BigDecimal(book.getOurPrice()).multiply(new BigDecimal(qty)));
+		cartItem = cartItemRepository.save(cartItem);
+		
+		BookToCartItem bookToCartItem = new BookToCartItem();
+		bookToCartItem.setBook(book);
+		bookToCartItem.setCartItem(cartItem);
+		bookToCartItemRepository.save(bookToCartItem);
+		
+		return cartItem;
+	}
+	
+	public CartItem findById(Long id) {
+		return cartItemRepository.findById(id).orElse(null);
+	}
+	
+	public void removeCartItem(CartItem cartItem) {
+		bookToCartItemRepository.deleteByCartItem(cartItem);
+		cartItemRepository.delete(cartItem);
+	}
+
+}
---
+++ src/main/java/com/eCommerce/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,44 @@
+package com.eCommerce.service.impl;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eCommerce.domain.CartItem;
+import com.eCommerce.domain.ShoppingCart;
+import com.eCommerce.repository.ShoppingCartRepository;
+import com.eCommerce.service.CartItemService;
+import com.eCommerce.service.ShoppingCartService;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService{
+	
+	@Autowired
+	private CartItemService cartItemService;
+	
+	@Autowired
+	private ShoppingCartRepository shoppingCartRepository;
+	
+	public ShoppingCart updateShoppingCart(ShoppingCart shoppingCart) {
+		BigDecimal cartTotal = new BigDecimal(0);
+		
+		List<CartItem> cartItemList = cartItemService.findByShoppingCart(shoppingCart);
+		
+		for (CartItem cartItem : cartItemList) {
+			if(cartItem.getBook().getInStockNumber() > 0) {
+				cartItemService.updateCartItem(cartItem);
+				cartTotal = cartTotal.add(cartItem.getSubtotal());
+			}
+		}
+		
+		shoppingCart.setGrandTotal(cartTotal);
+		
+		shoppingCartRepository.save(shoppingCart);
+		
+		return shoppingCart;
+	}
+
+}
+
