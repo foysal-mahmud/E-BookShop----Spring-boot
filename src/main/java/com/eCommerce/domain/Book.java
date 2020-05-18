f5405a5d8aad7c2588c84b463bf1a4892181c36f
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-18 20:48:49 +0600

    Update Book.java

--- src/main/java/com/eCommerce/domain/Book.java
+++ src/main/java/com/eCommerce/domain/Book.java
@@ -1,13 +1,18 @@
 package com.eCommerce.domain;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Transient;
 
 import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Book {
@@ -35,6 +40,11 @@ public class Book {
 	
 	@Transient
 	private MultipartFile bookImage;
+	
+	@OneToMany(mappedBy = "book")
+	@JsonIgnore
+	private List<BookToCartItem> bookToCartItemList;
+	
 
 	public Long getId() {
 		return id;
