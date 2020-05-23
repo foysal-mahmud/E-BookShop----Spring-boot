4b4679ee8b70efae96773f91f5a4148ed9210a42
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-23 10:06:37 +0600

    Add files via upload

---
+++ src/main/java/com/eCommerce/service/impl/OrderServiceImpl.java
@@ -0,0 +1,68 @@
+package com.eCommerce.service.impl;
+
+import java.util.Calendar;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eCommerce.domain.BillingAddress;
+import com.eCommerce.domain.Book;
+import com.eCommerce.domain.CartItem;
+import com.eCommerce.domain.Order;
+import com.eCommerce.domain.Payment;
+import com.eCommerce.domain.ShippingAddress;
+import com.eCommerce.domain.ShoppingCart;
+import com.eCommerce.domain.User;
+import com.eCommerce.repository.OrderRepository;
+import com.eCommerce.service.CartItemService;
+import com.eCommerce.service.OrderService;
+
+
+@Service
+public class OrderServiceImpl implements OrderService{
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CartItemService cartItemService;
+	
+	public synchronized Order createOrder(ShoppingCart shoppingCart,
+			ShippingAddress shippingAddress,
+			BillingAddress billingAddress,
+			Payment payment,
+			String shippingMethod,
+			User user) {
+		Order order = new Order();
+		order.setBillingAddress(billingAddress);
+		order.setOrderStatus("created");
+		order.setPayment(payment);
+		order.setShippingAddress(shippingAddress);
+		order.setShippingMethod(shippingMethod);
+		
+		List<CartItem> cartItemList = cartItemService.findByShoppingCart(shoppingCart);
+		
+		for(CartItem cartItem : cartItemList) {
+			Book book = cartItem.getBook();
+			cartItem.setOrder(order);
+			book.setInStockNumber(book.getInStockNumber() - cartItem.getQty());
+		}
+		
+		order.setCartItemList(cartItemList);
+		order.setOrderDate(Calendar.getInstance().getTime());
+		order.setOrderTotal(shoppingCart.getGrandTotal());
+		shippingAddress.setOrder(order);
+		billingAddress.setOrder(order);
+		payment.setOrder(order);
+		order.setUser(user);
+		order = orderRepository.save(order);
+		
+		return order;
+	}
+	
+	public Order findById(Long id) {
+		return orderRepository.findById(id).orElse(null);
+	}
+
+}
