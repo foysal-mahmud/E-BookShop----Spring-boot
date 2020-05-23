1d40b07342ba279b6896218a526c6a4326b75d70
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-23 10:05:30 +0600

    Add files via upload

--- src/main/java/com/eCommerce/service/ShoppingCartService.java
+++ src/main/java/com/eCommerce/service/ShoppingCartService.java
@@ -4,4 +4,6 @@ import com.eCommerce.domain.ShoppingCart;
 
 public interface ShoppingCartService {
 	ShoppingCart updateShoppingCart(ShoppingCart shoppingCart);
+	
+	void clearShoppingCart(ShoppingCart shoppingCart);
 }
