6e1737912280cf93c9fe9d4848e7f68bf7448893
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-23 10:09:43 +0600

    Update MailConstructor.java

--- src/main/java/com/eCommerce/utility/MailConstructor.java
+++ src/main/java/com/eCommerce/utility/MailConstructor.java
@@ -2,15 +2,28 @@ package com.eCommerce.utility;
 
 import java.util.Locale;
 
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import com.eCommerce.domain.Order;
 import com.eCommerce.domain.User;
+
 
 @Component
 public class MailConstructor {
+	
+	@Autowired
+	private TemplateEngine templateEngine;
+	
 	@Autowired
 	private Environment env;
 	
@@ -22,10 +35,33 @@ public class MailConstructor {
 		String message = "\nPlease click on this link to verify your email and edit your personal information. Your password is: \n"+password;
 		SimpleMailMessage email = new SimpleMailMessage();
 		email.setTo(user.getEmail());
-		email.setSubject("Le's Bookstore - New User");
+		email.setSubject("E-Commerce - New User");
 		email.setText(url+message);
 		email.setFrom(env.getProperty("support.email"));
 		return email;
 		
 	}
+	
+	public MimeMessagePreparator constructOrderConfirmationEmail (User user, Order order, Locale locale) {
+		Context context = new Context();
+		context.setVariable("order", order);
+		context.setVariable("user", user);
+		context.setVariable("cartItemList", order.getCartItemList());
+		String text = templateEngine.process("orderConfirmationEmailTemplate", context);
+		
+		MimeMessagePreparator messagePreparator = new MimeMessagePreparator() {
+			@Override
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				MimeMessageHelper email = new MimeMessageHelper(mimeMessage);
+				email.setTo(user.getEmail());
+				email.setSubject("Order Confirmation - "+order.getId());
+				email.setText(text, true);
+				email.setFrom(new InternetAddress("foysal.ecommerce@gmail.com"));
+			}
+		};
+		
+		return messagePreparator;
+	}
+	
+	
 }
