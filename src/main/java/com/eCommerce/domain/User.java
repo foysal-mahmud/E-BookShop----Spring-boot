7884b7462b2d76a4289824bd808ff8934f4945ee
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-24 19:47:25 +0600

    Update User.java

--- src/main/java/com/eCommerce/domain/User.java
+++ src/main/java/com/eCommerce/domain/User.java
@@ -45,9 +45,11 @@ public class User implements UserDetails{
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
 	private List<UserShipping> userShippingList;
 	
-	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
 	private List<UserPayment> userPaymentList;
+	
+	@OneToMany(mappedBy = "user")
+	private List<Order> orderList;
 	
 	@OneToMany(mappedBy = "user", cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
 	@JsonIgnore
@@ -125,6 +127,13 @@ public class User implements UserDetails{
 	}
 	public void setShoppingCart(ShoppingCart shoppingCart) {
 		this.shoppingCart = shoppingCart;
+	}
+	
+	public List<Order> getOrderList() {
+		return orderList;
+	}
+	public void setOrderList(List<Order> orderList) {
+		this.orderList = orderList;
 	}
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
