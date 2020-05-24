2f2e6c158e3bee8814c931fdbff30df0e6d4f31f
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-24 19:45:55 +0600

    Update UserServiceImpl.java

--- src/main/java/com/eCommerce/service/impl/UserServiceImpl.java
+++ src/main/java/com/eCommerce/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.eCommerce.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -7,7 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.eCommerce.domain.ShoppingCart;
 import com.eCommerce.domain.User;
 import com.eCommerce.domain.UserBilling;
 import com.eCommerce.domain.UserPayment;
@@ -59,11 +62,17 @@ public class UserServiceImpl implements UserService{
 	}
 	
 	@Override
+	public User findById(Long id){
+		return userRepository.findById(id).orElse(null);
+	}
+	
+	@Override
 	public User findByEmail (String email) {
 		return userRepository.findByEmail(email);
 	}
 	
 	@Override
+	@Transactional
 	public User createUser(User user, Set<UserRole> userRoles){
 		User localUser = userRepository.findByUsername(user.getUsername());
 		
@@ -75,6 +84,13 @@ public class UserServiceImpl implements UserService{
 			}
 			
 			user.getUserRoles().addAll(userRoles);
+			
+			ShoppingCart shoppingCart = new ShoppingCart();
+			shoppingCart.setUser(user);
+			user.setShoppingCart(shoppingCart);
+			
+			user.setUserShippingList(new ArrayList<UserShipping>());
+			user.setUserPaymentList(new ArrayList<UserPayment>());
 			
 			localUser = userRepository.save(user);
 		}
@@ -110,7 +126,6 @@ public class UserServiceImpl implements UserService{
 		List<UserPayment> userPaymentList = (List<UserPayment>) userPaymentRepository.findAll();
 		
 		for (UserPayment userPayment : userPaymentList) {
-			
 			if(userPayment.getId() == userPaymentId) {
 				userPayment.setDefaultPayment(true);
 				userPaymentRepository.save(userPayment);
@@ -135,6 +150,5 @@ public class UserServiceImpl implements UserService{
 			}
 		}
 	}
-
 
 }
