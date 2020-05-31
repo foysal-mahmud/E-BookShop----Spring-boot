3fd812c718264dbc84a3162e159194b65d7e7194
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-31 18:20:29 +0600

    Update ECommerceApplication.java

--- src/main/java/com/eCommerce/ECommerceApplication.java
+++ src/main/java/com/eCommerce/ECommerceApplication.java
@@ -25,20 +25,5 @@ public class ECommerceApplication implements CommandLineRunner {
 		SpringApplication.run(ECommerceApplication.class, args);
 	}
 	
-	@Override
-	public void run(String... args) throws Exception {
-		User user1 = new User();
-		user1.setFirstName("John");
-		user1.setLastName("Adams");
-		user1.setUsername("j");
-		user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
-		user1.setEmail("JAdams@gmail.com");
-		Set<UserRole> userRoles = new HashSet<>();
-		Role role1= new Role();
-		role1.setRoleId(1);
-		role1.setName("ROLE_USER");
-		userRoles.add(new UserRole(user1, role1));
-		
-		userService.createUser(user1, userRoles);
-	}
+
 }
