b02f59c07c6d16ee365d4fbb27209d6c786e1ada
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-31 18:41:57 +0600

    Update BookRepository.java

--- src/main/java/com/eCommerce/repository/BookRepository.java
+++ src/main/java/com/eCommerce/repository/BookRepository.java
@@ -1,10 +1,16 @@
 package com.eCommerce.repository;
 
 
-import com.eCommerce.domain.Book;
+import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.eCommerce.domain.Book;
+
 public interface BookRepository extends CrudRepository<Book, Long>{
+	
+List<Book> findByCategory(String category);
+	
+	List<Book> findByTitleContaining(String title);
 
 }
