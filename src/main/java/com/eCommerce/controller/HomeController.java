1e03fe17c8dd69f2a76aa894ae0ffd6babd84e70
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-31 18:18:48 +0600

    Finishing up

--- src/main/java/com/eCommerce/controller/HomeController.java
+++ src/main/java/com/eCommerce/controller/HomeController.java
@@ -90,10 +90,23 @@ public class HomeController {
 		return "myAccount";
 	}
 	
+	@RequestMapping("/faq")
+	public String faq() {
+		return "faq";
+	}
+	
 	@RequestMapping("/bookshelf")
-	public String bookshelf(Model model) {
+	public String bookshelf(Model model, Principal principal) {
+		
+		if(principal != null) {
+			String username = principal.getName();
+			User user = userService.findByUsername(username);
+			model.addAttribute("user", user);
+		}
+		
 		List<Book> bookList = bookService.findAll();
 		model.addAttribute("bookList", bookList);
+		model.addAttribute("activeAll",true);
 		
 		return "bookshelf";
 	}
