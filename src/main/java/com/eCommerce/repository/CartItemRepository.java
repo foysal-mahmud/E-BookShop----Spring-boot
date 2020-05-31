f0d50b3017ceb504c70282e10b36c7ee5491de78
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-31 18:43:50 +0600

    Update CartItemRepository.java

--- src/main/java/com/eCommerce/repository/CartItemRepository.java
+++ src/main/java/com/eCommerce/repository/CartItemRepository.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 import com.eCommerce.domain.CartItem;
+import com.eCommerce.domain.Order;
 import com.eCommerce.domain.ShoppingCart;
 
 
 public interface CartItemRepository extends CrudRepository<CartItem, Long>{
+	
 	List<CartItem> findByShoppingCart(ShoppingCart shoppingCart);
+	
+	List<CartItem> findByOrder(Order order);
 }
