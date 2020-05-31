8c2ba01ee0e66cd35b3359ef24a3a371bc813842
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-31 18:41:11 +0600

    Update UserShipping.java

--- src/main/java/com/eCommerce/domain/UserShipping.java
+++ src/main/java/com/eCommerce/domain/UserShipping.java
@@ -19,6 +19,7 @@ public class UserShipping {
 	private String userShippingState;
 	private String userShippingCountry;
 	private String userShippingZipcode;
+	private boolean userShippingDefault;
 	
 	
 	@ManyToOne
@@ -114,6 +115,18 @@ public class UserShipping {
 	public void setUser(User user) {
 		this.user = user;
 	}
+
+
+	public boolean isUserShippingDefault() {
+		return userShippingDefault;
+	}
+
+
+	public void setUserShippingDefault(boolean userShippingDefault) {
+		this.userShippingDefault = userShippingDefault;
+	}
+	
+	
 	
 	
 }
