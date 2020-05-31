e910e70cf1c1e41d37bffcad7f582234a37a63c6
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-05-31 18:30:32 +0600

    Update Role.java

--- src/main/java/com/eCommerce/domain/security/Role.java
+++ src/main/java/com/eCommerce/domain/security/Role.java
@@ -16,7 +16,7 @@ public class Role {
 	private int roleId;
 	private String name;
 	
-	@OneToMany(mappedBy = "role", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+	@OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch=FetchType.LAZY)
 	private Set<UserRole> userRoles = new HashSet<>();
 
 	public int getRoleId() {
