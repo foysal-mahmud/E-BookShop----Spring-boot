64979f99b85171173db1a8d863618bdc2c50b88d
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-06-01 11:02:10 +0600

    Update BookServiceImpl.java

--- src/main/java/com/eCommerce/service/impl/BookServiceImpl.java
+++ src/main/java/com/eCommerce/service/impl/BookServiceImpl.java
@@ -1,5 +1,6 @@
 package com.eCommerce.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +16,47 @@ public class BookServiceImpl implements BookService{
 	private BookRepository bookRepository;
 	
 	public List<Book> findAll() {
-		return (List<Book>) bookRepository.findAll();
+		List<Book> bookList = (List<Book>) bookRepository.findAll();
+		List<Book> activeBookList = new ArrayList<>();
+		
+		for (Book book: bookList) {
+			if(book.isActive()) {
+				activeBookList.add(book);
+			}
+		}
+		
+		return activeBookList;
 	}
 	
 	public Book findById(Long id) {
 		return bookRepository.findById(id).orElse(null);
+	}
+	
+	public List<Book> findByCategory(String category){
+		List<Book> bookList = bookRepository.findByCategory(category);
+		
+		List<Book> activeBookList = new ArrayList<>();
+		
+		for (Book book: bookList) {
+			if(book.isActive()) {
+				activeBookList.add(book);
+			}
+		}
+		
+		return activeBookList;
+	}
+	
+	public List<Book> blurrySearch(String title) {
+		List<Book> bookList = bookRepository.findByTitleContaining(title);
+        List<Book> activeBookList = new ArrayList<>();
+		
+		for (Book book: bookList) {
+			if(book.isActive()) {
+				activeBookList.add(book);
+			}
+		}
+		
+		return activeBookList;
 	}
 
 }
