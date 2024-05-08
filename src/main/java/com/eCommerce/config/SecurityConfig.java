66464dfc5b50c498dec819d04b157c8b4ca5a52c
hendisantika <hendisantika@yahoo.co.id>
2024-05-08 07:25:58 +0700

    #3 | Hendi Santika | Adjust Spring Security ✏️📗 :octocat: 🐳⬆

--- src/main/java/com/eCommerce/config/SecurityConfig.java
+++ src/main/java/com/eCommerce/config/SecurityConfig.java
@@ -1,24 +1,25 @@
 package com.eCommerce.config;
 
+import com.eCommerce.service.impl.UserSecurityService;
+import com.eCommerce.utility.SecurityUtility;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import com.eCommerce.service.impl.UserSecurityService;
-import com.eCommerce.utility.SecurityUtility;
 
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled=true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableMethodSecurity
+public class SecurityConfig {
 	@Autowired
 	private Environment env;
 
@@ -45,24 +46,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			"/searchBook"
 	};
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests().
-		/*	antMatchers("/**").*/
-			antMatchers(PUBLIC_MATCHERS).
-			permitAll().anyRequest().authenticated();
-
-		http
-			.csrf().disable().cors().disable()
-			.formLogin().failureUrl("/login?error")
-			/*.defaultSuccessUrl("/")*/
-			.loginPage("/login").permitAll()
-			.and()
-			.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-			.logoutSuccessUrl("/?logout").deleteCookies("remember-me").permitAll()
-			.and()
-			.rememberMe();
+				.cors(AbstractHttpConfigurer::disable)
+				.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(authz -> authz
+						.requestMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated()
+				)
+				.formLogin(formLogin -> formLogin
+						.loginPage("/login")
+						.defaultSuccessUrl("/", true)
+						.failureUrl("/login?error")
+				)
+				.logout(logout -> logout
+						.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+						.logoutSuccessUrl("/?logout").deleteCookies("remember-me").permitAll()
+				);
+		return http.build();
 	}
 
 	@Autowired
