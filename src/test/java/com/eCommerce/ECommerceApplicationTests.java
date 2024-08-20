4cb0bb0ecdf3a80d2ee75924fe6b2f8d088e5a9f
hendisantika <hendisantika@yahoo.co.id>
2024-08-20 19:39:01 +0700

    #5 | Hendi Santika | Setup GitHub Action :two:🫸🌀✏️📗🐧🐳⬆

--- src/test/java/com/eCommerce/ECommerceApplicationTests.java
+++ src/test/java/com/eCommerce/ECommerceApplicationTests.java
@@ -1,13 +1,29 @@
 package com.eCommerce;
 
-import org.junit.jupiter.api.Test;
+import com.eCommerce.repository.BookRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+		properties = {
+				"management.endpoint.health.show-details=always",
+				"spring.datasource.url=jdbc:tc:mysql:8.4.0:///bookShopDB"
+		},
+		webEnvironment = RANDOM_PORT
+)
 public class ECommerceApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Autowired
+	private BookRepository bookRepository;
+
+	@BeforeEach
+	void deleteAll() {
+		bookRepository.deleteAll();
 	}
 
 }
